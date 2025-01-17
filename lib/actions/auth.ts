8e69ce37d9d0bb11f849ddbb898ccff917610522
Anthony David <anthony.david34@sfr.fr>
2025-01-17 10:07:54 +0100

    setup Upstash for rate limit

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -6,10 +6,10 @@ import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
 import { headers } from "next/headers";
-//import ratelimit from "@/lib/ratelimit";
+import ratelimit from "@/lib/ratelimit";
 import { redirect } from "next/navigation";
 //import { workflowClient } from "@/lib/workflow";
-import config from "@/lib/config";
+//import config from "@/lib/config";
 
 export const signInWithCredentials = async (
   params: Pick<AuthCredentials, "email" | "password">
@@ -17,10 +17,10 @@ export const signInWithCredentials = async (
   const { email, password } = params;
 
   const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-  /*   const { success } = await ratelimit.limit(ip);
+  const { success } = await ratelimit.limit(ip);
 
   if (!success) return redirect("/too-fast");
- */
+
   try {
     const result = await signIn("credentials", {
       email,
@@ -43,9 +43,9 @@ export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, password } = params;
 
   const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
-  /*   const { success } = await ratelimit.limit(ip);
+  const { success } = await ratelimit.limit(ip);
 
-  if (!success) return redirect("/too-fast"); */
+  if (!success) return redirect("/too-fast");
 
   const existingUser = await db
     .select()
